afb6d552936da0fe77b6d39c99e5b08240df96e4
Sakthivel J <98632696+sakthivelgreen@users.noreply.github.com>
2024-12-06 17:37:41 +0530

    add: Drag and Drop

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 import { _var } from "./variables.js";
 
-let files;
+let files = [];
 
 main(); // main Function call
 
@@ -9,27 +9,36 @@ function main() { // Main Function Declaration
 }
 
 function Events() {
-    _var.drop_div().addEventListener('click', (e) => {
+    _var.drop_zone().addEventListener('click', (e) => {
         e.stopImmediatePropagation();
         _var.file_input().click();
     });
+    _var.drop_zone().addEventListener('dragover', (e) => {
+        e.preventDefault();
+    })
+    _var.drop_zone().addEventListener('drop', (e) => {
+        e.preventDefault();
+        if (e.dataTransfer.items) {
+            [...e.dataTransfer.items].forEach(item => {
+                if (item.kind == 'file') {
+                    let file = item.getAsFile();
+                    files.push(file);
+                }
+            });
+            enable_file_options();
+            e.dataTransfer.items.clear();
+        }
+    })
     _var.file_input().addEventListener('change', (e) => {
         files = e.target.files;
-        if ((files.length > 0) && (files[0].size <= (10 * 1024 * 1024))) {
-            _var.file_name().textContent = files[0].name.split('.')[0];
-            let file_size = files[0].size >= (1024 * 1024) ? `${(files[0].size / (1024 * 1024)).toFixed(2)
-                } MB` : `${(files[0].size / 1024).toFixed(2)} KB`;
-            _var.file_details().textContent = `.${files[0].name.split('.')[1]} | ${file_size}`;
-            _var.file_show().style.display = 'flex';
-        } else {
-            alert('Maximum file size should be 10 MB')
-        }
+        enable_file_options();
     })
     _var.delete_file().addEventListener('click', (e) => {
         _var.file_input.value = '';
         _var.file_name().textContent = '';
         _var.file_details().textContent = '';
         _var.file_show().style.display = 'none';
+        files = [];
     })
     _var.download_file().addEventListener('click', (e) => {
         let obj = URL.createObjectURL(files[0]);
@@ -39,4 +48,16 @@ function Events() {
         a.click();
         URL.revokeObjectURL(obj);
     })
+}
+
+function enable_file_options() {
+    if ((files.length > 0) && (files[0].size <= (10 * 1024 * 1024))) {
+        _var.file_name().textContent = files[0].name.split('.')[0];
+        let file_size = files[0].size >= (1024 * 1024) ? `${(files[0].size / (1024 * 1024)).toFixed(2)
+            } MB` : `${(files[0].size / 1024).toFixed(2)} KB`;
+        _var.file_details().textContent = `.${files[0].name.split('.').slice(-1)[0]} | ${file_size}`;
+        _var.file_show().style.display = 'flex';
+    } else {
+        alert('Maximum file size should be 10 MB')
+    }
 }
--- variables.js
+++ variables.js
@@ -1,5 +1,5 @@
 export const _var = {
-    drop_div: () => document.querySelector('.drop-zone'),
+    drop_zone: () => document.querySelector('.drop-zone'),
     file_input: () => document.querySelector('#file'),
     file_show: () => document.querySelector('.uploaded-item-div'),
     file_name: () => document.querySelector('#file-name'),
